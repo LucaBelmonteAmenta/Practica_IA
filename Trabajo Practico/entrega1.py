90eb55adfcd527ad5d6d055e2e9e6cbdb9c212c5
Lucas_Belmonte_Amenta <lucasbelmonteamenta2.0@gmail.com>
2020-10-23 16:53:20 -0300

    agregue los detalles iniciales del TP (leer descripcion del commit)
    agregue los nombre de los metodos y clases basicas a implementar, corregí los nombres para que coincidan con lo planteado en la entrega y carguí el test que el profe nos pasó.

---
+++ Trabajo Practico/entrega1.py
@@ -0,0 +1,28 @@
+from simpleai.search import SearchProblem
+
+
+class MercadoArtificialProblem(SearchProblem):
+  
+    def cost(self, state1, action, state2):
+        pass
+
+
+    def is_goal(self, state):
+        pass
+
+
+    def actions(self, state):       
+        pass
+
+
+    def result(self, state, action):       
+        pass
+
+
+    def heuristic(self,state):
+        pass
+
+
+
+def planear_camiones():
+    pass
